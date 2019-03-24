e52054c824d2507b0fce98362e4ed11eecc44b5c
sencondly <dmzsecondly@gmail.com>
2019-03-24 23:53:21 +0900

    update: directory structure

--- bookstore_grpc/mysql/initdb.d/1_init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "GRANT ALL ON \`api\`.* TO '"$MYSQL_USER"'@'%' ;" | "${mysql[@]}"
-echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
-
-"${mysql[@]}" < /docker-entrypoint-initdb.d/1_schema.sql_
